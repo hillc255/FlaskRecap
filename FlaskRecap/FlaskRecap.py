f3d79717ce3231a71c7e87c2bf78c3e84bf265cf
Claudia Hill <hillc255@yahoo.com>
2021-01-17 14:35:00 -0500

    add access token code

--- FlaskRecap/FlaskRecap.py
+++ FlaskRecap/FlaskRecap.py
@@ -1,6 +1,55 @@
 from flask import Flask, request, jsonify, abort
+from functools import wraps
 
+def requires_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        jwt = get_token_auth_header()
+        return f(jwt, *args, **kwargs)
+    return wrapper
 app = Flask(__name__)
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description":
+                            "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+			
+@app.route('/headers')
+@requires_auth
+def headers(jwt):
+    # @TODO unpack the request header
+    print(jwt)
+    return 'not implemented'
+
+@app.route('/images')
+@requires_auth
+def images(jwt):
+    # @TODO unpack the request header
+    print(jwt)
+    return 'not implemented'
 
 greetings = {
             'en': 'hello', 
